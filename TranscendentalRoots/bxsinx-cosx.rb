62159a2af7cc87673819273cfd9325be969db688
Christoph Ruegg <git@cdrnet.ch>
2009-03-31 12:09:58 +0200

    converted to yield block

--- TranscendentalRoots/bxsinx-cosx.rb
+++ TranscendentalRoots/bxsinx-cosx.rb
@@ -3,12 +3,13 @@
 # equation: b*x*cos(x) - sin(x) = 0
 # rewritten as: y = b*x = tan(x)
 
-def iterate_root(refine, x)
+def refine_root(x)
   (1..50).each do |i|
     xold = x
-    x = refine.call(x)
+    x = yield(x)
     xm = [x.abs,xold.abs].max
     if xm == 0 || (x-xold).abs/xm < 1e-14
+      printf "%02d iterations:", i
       break
     end
   end
@@ -24,8 +25,11 @@ if __FILE__ == $0
     
     (-2..2).each do |n|
       
-      refine = lambda { |x| Math::PI*n + Math.atan(b*x) }
-      x = iterate_root(refine, n*Math::PI)
+      x = refine_root(n*Math::PI) do |x|
+        y = b*x
+        Math::PI*n + Math.atan(y)
+      end
+      
       puts " %2d: %s" % [n,x]
       
     end
