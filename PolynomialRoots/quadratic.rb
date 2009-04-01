1c4779940305bebdf4f9ea1ac5df09233421c3c8
Christoph Ruegg <git@cdrnet.ch>
2009-04-01 22:23:31 +0200

    quadratic roots: code refactoring

--- PolynomialRoots/quadratic.rb
+++ PolynomialRoots/quadratic.rb
@@ -28,63 +28,77 @@ def robust_division(a, b)
       return Complex(a * r / den, -a / den)
     end
   end
-
 end
 
-def schoolbook_formula(b, c)
-  
-  sqrtarg = Complex(b**2 - c, 0)
+def refine_root(x)
+  xorig = x
+  (0..5).each do |i|
+    xold = x
+    x = yield(x)
+    if (x.real-xold.real == 0) && (x.image-xold.image == 0)
+      puts "  #{i} iterations"
+      return x 
+    end
+  end
+  puts "not converged"
+  return xorig
+end
+
+@schoolbook_formula = lambda {|b, c|
+  sqrtarg = b*b - c
   sqrt = Math.sqrt(sqrtarg)
   x1 = -b + sqrt
   x2 = -b - sqrt
-  
-  return [x1, x2]
-  
-end
+  [x1, x2]
+}
 
-def avoid_subtraction(b, c)
-  
-  sqrtarg = Complex(b**2 - c, 0)
+@avoid_subtraction_formula = lambda {|b, c|
+  sqrtarg = b*b - c
   sqrt = Math.sqrt(sqrtarg)
   if b > 0
     x2 = -b - sqrt
-    #~ x1 = c / x2
-    x1 = robust_division c, x2
+    x1 = c / x2
+    #~ x1 = robust_division c, x2
   else
     x1 = -b + sqrt
-    #~ x2 = c / x1
-    x2 = robust_division c, x1
+    x2 = c / x1
+    #~ x2 = robust_division c, x1
   end
-  
-  return [x1, x2]
-  
-end
+  [x1, x2]
+}
 
-def evaluate(b, c, x)
-  x**2 + 2*b*x + c
+@refined_formula = lambda {|b, c|
+  @avoid_subtraction_formula[b, c].collect do |x|
+    refine_root(x) { |x| (x*x-c)/(2*(x+b)) }
+  end
+}
+
+def analyze_formula(b, c)
+  #~ x1, x2 = forumla[b, c]
+  roots = yield(b, c)
+  puts "  Roots: %s, %s" % roots
+  puts "  Result: %s, %s" % roots.map {|x| (x + 2 * b) * x + c }
 end
 
 def analyze_roots(b, c)
-  puts "b=%.1e, c=%.1e" % [b, c]
+  puts "b=#{b}, c=#{c}"
   
   puts "Schoolbook Formula"
-  roots_a = schoolbook_formula b, c
-  puts "Roots: %s, %s" % roots_a
-  puts "Result: %s, %s" % [evaluate(b, c, roots_a[0]), evaluate(b, c, roots_a[1])]
+  analyze_formula b, c, &@schoolbook_formula
   
-  puts "Avoid Subtraction"
-  roots_b = avoid_subtraction b, c
-  puts "Roots: %s, %s" % roots_b
-  puts "Result: %s, %s" % [evaluate(b, c, roots_b[0]), evaluate(b, c, roots_b[1])]
+  puts "Avoid Subtraction Formula"
+  analyze_formula b, c, &@avoid_subtraction_formula
+  
+  puts "Refined Formula"
+  analyze_formula b, c, &@refined_formula
 end
 
+
 if __FILE__ == $0
-  
   puts
   analyze_roots 10.0**4, 4.0
   puts
   analyze_roots -5.0, 1.0
   puts
   analyze_roots 10.0**12, -10.0**14
-  
 end
